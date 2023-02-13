8053302cb70b8c6f646fe8b97e21975f539da0c6
Y127908 <Sergey.starostin@vaz.ru>
2023-02-13 15:51:27 +0400

    Программа вывода на экран четных чисел от 1-N

---
+++ Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число N: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());   // вводим число N
+
+       int count = 1;                               // начало отсчета
+       while (count < Number)                       // пока count < Number, цикл выполняется
+       {
+            System.Console.Write($"{count +1} | "); // вывод на экран четное число
+            count += 2;                             // увеличиваем count на 2
+       }
