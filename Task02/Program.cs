c13ee2c828e9fbdd382ea25cc2a3209d8ce1e603
Y127908 <Sergey.starostin@vaz.ru>
2023-02-13 14:15:36 +0400

    Программа поиска max из трех введенных чисел

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+System.Console.WriteLine("Введите три числа:");
+
+int Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = Convert.ToInt32(Console.ReadLine());
+int Number3 = Convert.ToInt32(Console.ReadLine());
+int max = Number1;
+
+if (Number1 < Number2)
+{
+    max = Number2;
+    if (max < Number3)
+    {
+        max = Number3;
+    }
+    System.Console.Write($"Максимальное число: {max}");
+}
+else
+    System.Console.Write($"Максимальное число: {max}");
