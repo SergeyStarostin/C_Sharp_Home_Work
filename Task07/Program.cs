7ac044b74d99216e805c12efee5ea145cd4c9d99
Starostin SV <Vlillen@yandex.ru>
2023-02-19 13:17:16 +0400

    Программа, которая проверяет по введенной цифре, является этот ли день выходным или нет

---
+++ Task07/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool WeekDay(int Day)
+{
+    if (Day > 5 && Day < 8)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool TheDayOfWeek(int Day)
+{
+    if (Day > 0 && Day < 8)
+    {
+        return true;
+    }
+        return false;
+}
+
+int Day = Prompt ("Введите день недели: ");
+
+if (TheDayOfWeek(Day))
+{
+    if (WeekDay(Day))
+    {
+        Console.WriteLine ($"{Day} день выходной");
+    }
+    else
+    {
+        Console.WriteLine ($"{Day} будний день");
+    }
+}
+else
+    {
+        Console.WriteLine ($"{Day} день недели еще не придумали");
+    }
