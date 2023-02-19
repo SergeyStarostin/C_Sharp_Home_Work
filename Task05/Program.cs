dd7d553a120acfb025a4777402b7c27320e7ff0a
Starostin SV <Vlillen@yandex.ru>
2023-02-19 13:31:08 +0400

    Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Решение без использования функций.

---
+++ Task05/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// //вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 99 && Number < 1000)
+{
+    int SecondNumber = Number % 100 / 10;
+    System.Console.Write($"Вторая цифра трехзначного числа = {SecondNumber}");
+}
+else
+{
+    System.Console.Write($"{Number} не является трехзначным");
+}     
---
+++ Task06.1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int ThirdNumber (int Number)
+
+{
+    while (Number > 999)
+    {
+        Number /= 10;
+    }
+    return Number % 10;
+}
+
+bool ValidateNumber (int Number)
+{
+    if (Number < 100)
+    {
+        System.Console.Write($"в числе {Number} нет третьей цифры");
+        return false;
+    }
+    return true;
+}
+
+int Number = Prompt ("Введите число: ");
+
+if (ValidateNumber(Number))
+{
+    Console.WriteLine(ThirdNumber(Number));
+}
