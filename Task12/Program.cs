f15444fd0ff4cc7cbf8fc57d9be8b67fbddc79f6
Starostin SV <Vlillen@yandex.ru>
2023-02-27 22:48:24 +0400

    Сумма цифр в числе

---
+++ Task12/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int SumOfDigits (int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt($"Введите число: ");
+Console.WriteLine($"Сумма цифр в числе: {number} = {SumOfDigits (number)}");
