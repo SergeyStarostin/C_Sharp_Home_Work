85bc41ae97d44e76074980ea2fc9bad11919f12d
Starostin SV <Vlillen@yandex.ru>
2023-03-06 22:37:20 +0400

    заполнение двумерного массива размером m*n случайными вещественными числами.

---
+++ Task24/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5    7    -2   -0,2
+//  1   -3,3    8   -9,9
+//  8    7,8  -7,1    9
+
+int Prompt (string message) // метод считывания чисел с консоли
+{
+    Console.Write(message);
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value)) // проверка условия корректности ввода
+    {
+        return value;
+    }
+    Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+void FillArrayRandomNum(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 100;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int rows = Prompt("Введите количество строк:  ");
+int cols = Prompt("Введите количество столбцов: ");
+double[,] matrix = new double[rows, cols];
+FillArrayRandomNum(matrix);
+PrintArray(matrix);
